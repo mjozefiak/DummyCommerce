e92eb77ea8f45b08e082af014ce6b79a30b46548
Mikołaj Józefiak <>
2023-04-21 13:53:59 +0200

    Hotfix: EmptyBasket.tsx

--- src/components/EmptyBasket.tsx
+++ src/components/EmptyBasket.tsx
@@ -1,10 +1,10 @@
 import {Link} from "react-router-dom";
-import {basketActions} from "../store/basket-slice";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../store/store";
+import {uiActions} from "../store/ui-slice";
 
 const EmptyBasket = () => {
-   const basketState = useSelector((state: RootState) => state.basket)
+   const uiState = useSelector((state: RootState) => state.ui)
    const dispatch = useDispatch()
 
    return (
@@ -14,7 +14,7 @@ const EmptyBasket = () => {
          <p className="text-center mt-1 text-gray-500">Look you haven't added anything to your cart yet</p>
          <Link
             to='/'
-            onClick={() => basketState.isVisible && dispatch(basketActions.toggleVisibility())}
+            onClick={() => uiState.basketState && dispatch(uiActions.toggleBasketState())}
             className="bg-blue-600 text-white py-2 px-5 rounded mt-4"
          >
             Shop now
